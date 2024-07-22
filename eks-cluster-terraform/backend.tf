ec5b802ff0ebc55d614a1f5c73440e574ea5a372
Nishant Gupta <68242561+nishantg98@users.noreply.github.com>
2024-07-22 16:10:36 +0530

    Update backend.tf

--- eks-cluster-terraform/backend.tf
+++ eks-cluster-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "task-master-pro-eks-cluster-terraform"
-    key    = "task-master-pro/terraform.tfstate"
+    bucket = "task-master-eks-cluster-terraform"
+    key    = "task-master/terraform.tfstate"
     region = "us-east-1"
 
   }
